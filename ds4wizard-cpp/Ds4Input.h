8b8754c5c7e11fdc5d2f76680f99a0d933ab5b40
Michael Fadely <michaelwfadely@gmail.com>
2018-09-17 15:49:53 -0600

    cleanup

--- ds4wizard-cpp/Ds4Input.h
+++ ds4wizard-cpp/Ds4Input.h
@@ -12,29 +12,24 @@ class Ds4Input
 public:
 	Ds4Input() = default;
 	
+	bool buttonsChanged = false; // TODO: private set
+	bool touchChanged = false; // TODO: private set
 	Ds4Buttons_t heldButtons = 0; // TODO: private set
 	Ds4Buttons_t pressedButtons = 0; // TODO: private set
 	Ds4Buttons_t releasedButtons = 0; // TODO: private set
 
-private:
-	Ds4Buttons_t lastHeldButtons = 0;
-
-public:
-	bool buttonsChanged = false; // TODO: private set
-	bool touchChanged = false; // TODO: private set
-
 	Ds4InputData data {};
 	XInputGamepad gamepad {};
 
-private:
-	XInputGamepad lastGamepad {};
-	uint8_t lastTouchFrame {};
-
-	void addButton(bool pressed, Ds4Buttons_t buttons);
-
-public:
 	void update(const gsl::span<uint8_t>& buffer);
 	void updateChangedState();
 	void toXInput(int index, std::unique_ptr<ScpDevice>& device);
 	float getAxis(Ds4Axis_t axis, const std::optional<AxisPolarity>& polarity) const;
+
+private:
+	Ds4Buttons_t lastHeldButtons = 0;
+	XInputGamepad lastGamepad {};
+	uint8_t lastTouchFrame {};
+
+	void addButton(bool pressed, Ds4Buttons_t buttons);
 };
