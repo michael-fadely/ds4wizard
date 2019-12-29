91df13166e98d0262ce621aaa74f92b43689dc25
Michael Fadely <michaelwfadely@gmail.com>
2019-12-28 17:41:08 -0700

    fixed oversight

--- ds4wizard-cpp/Event.h
+++ ds4wizard-cpp/Event.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <deque>
+#include <unordered_map>
 #include <functional>
 #include <memory>
 
@@ -46,7 +46,10 @@ public:
 	 */
 	void remove(EventToken token)
 	{
-		callbacks.erase(std::remove(callbacks.begin(), callbacks.end(), token));
+		callbacks.erase(std::remove_if(callbacks.begin(), callbacks.end(), [token](auto ptr) -> bool
+		{
+			return ptr.lock() == token;
+		}));
 	}
 
 	/**
