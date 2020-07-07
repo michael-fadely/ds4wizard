22ae94a65ee96b7f67094c0bba66618bf2aed991
Michael Fadely <michaelwfadely@gmail.com>
2020-07-07 09:41:50 -0600

    fix latency on Windows 10 2004

--- ds4wizard-cpp/Ds4Device.cpp
+++ ds4wizard-cpp/Ds4Device.cpp
@@ -750,10 +750,8 @@ void Ds4Device::controllerThread()
 
 		if (!dataReceived)
 		{
-			std::this_thread::yield();
+			std::this_thread::sleep_for(1ms);
 		}
-
-		std::this_thread::sleep_for(1ms);
 	}
 
 	closeImpl();
