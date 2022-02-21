cec8717c70df14731c4634511583c71451bb2ecd
Michael Fadely <michaelwfadely@gmail.com>
2022-02-21 12:54:37 -0800

    upgrade ViGEmClient, use cmake to generate project, and upgrade to VS 2022

--- setup.bat
+++ setup.bat
@@ -1,9 +1,25 @@
 @echo off
-cd dependencies
-rmdir /s /q fmt-build
-mkdir fmt-build
-cd fmt-build
+setlocal EnableDelayedExpansion
 
-cmake -G "Visual Studio 16 2019" -A "x64" -D "CMAKE_CONFIGURATION_TYPES=Debug;Release" "..\fmt"
+:MAIN
+	pushd "%~dp0\dependencies"
 
-cd ..\..
+	call :MAKE_CMAKE_PROJECT "fmt"
+	call :MAKE_CMAKE_PROJECT "ViGEmClient"
+
+	popd
+goto :EOF
+
+@rem %~1 = Source directory name (assumes working directory is the dependencies directory)
+:MAKE_CMAKE_PROJECT
+	if exist "%~1-build" (
+		rmdir /s /q "%~1-build"
+	)
+
+	mkdir "%~1-build"
+	pushd "%~1-build"
+
+	cmake -G "Visual Studio 17 2022" -A "x64" -D "CMAKE_CONFIGURATION_TYPES=Debug;Release" "..\%~1"
+
+	popd
+	exit /b %ERRORLEVEL%
