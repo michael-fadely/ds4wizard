7f6ac957a0ff837d1606ca602e47b4552fb23678
Michael Fadely <michaelwfadely@gmail.com>
2022-02-21 13:41:27 -0800

    upgrade to C++20

--- libdevicetoggle/devicetoggle.cpp
+++ libdevicetoggle/devicetoggle.cpp
@@ -8,103 +8,90 @@
 
 #include "devicetoggle.h"
 
-std::string to_string(const std::wstring& wstr)
+std::string getLastErrorString(const DWORD error)
 {
-	return std::wstring_convert<std::codecvt_utf8<wchar_t>>().to_bytes(wstr);
+	if (error == ERROR_SUCCESS)
+	{
+		return {};
+	}
+
+	LPSTR messageBuffer = nullptr;
+
+	const DWORD size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+	                                  NULL,
+	                                  error,
+	                                  MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+	                                  reinterpret_cast<LPSTR>(&messageBuffer),
+	                                  0,
+	                                  NULL);
+
+	if (size < 1 && GetLastError() != ERROR_SUCCESS)
+	{
+		return {};
+	}
+
+	std::string message(messageBuffer, size);
+
+	LocalFree(messageBuffer);
+
+	return message;
 }
 
-void toggleDevice_doWork(const std::wstring& instanceId, const HDEVINFO devInfoSet)
+std::string getLastErrorString()
 {
-	SP_DEVINFO_DATA devInfoData;
-	devInfoData.cbSize = sizeof(SP_DEVINFO_DATA);
+	return getLastErrorString(GetLastError());
+}
 
-	if (!SetupDiEnumDeviceInfo(devInfoSet, 0, &devInfoData))
+inline void wideToStringError(int length)
+{
+	if (length < 1)
 	{
-		const DWORD error = GetLastError();
+		switch (GetLastError())
+		{
+			case ERROR_SUCCESS:
+				break;
 
-		std::stringstream message;
-		message << "Retrieving device info for instance ID "
-			<< to_string(instanceId) << " failed while attempting to toggle device with error code "
-			<< std::hex << std::setw(8) << std::setfill('0') << error;
+			default:
+				throw std::runtime_error(getLastErrorString());
+		}
+	}
+}
 
-		throw std::runtime_error(message.str());
+std::string wideToString(const std::wstring& wideString)
+{
+	if (!wideString.length())
+	{
+		return {};
 	}
 
-	SP_PROPCHANGE_PARAMS propChangeParams;
+	constexpr UINT codePage = CP_UTF8;
+	constexpr DWORD flags = WC_ERR_INVALID_CHARS;
 
-	propChangeParams.ClassInstallHeader.cbSize          = sizeof(SP_CLASSINSTALL_HEADER);
-	propChangeParams.ClassInstallHeader.InstallFunction = DIF_PROPERTYCHANGE;
+	const int requiredBufferSize = WideCharToMultiByte(codePage,
+	                                                   flags,
+	                                                   wideString.data(),
+	                                                   static_cast<int>(wideString.length()),
+	                                                   nullptr,
+	                                                   0,
+	                                                   NULL,
+	                                                   NULL);
 
-	propChangeParams.StateChange = DICS_DISABLE;
-	propChangeParams.HwProfile   = 0;
-	propChangeParams.Scope       = DICS_FLAG_GLOBAL;
+	wideToStringError(requiredBufferSize);
 
-	// Prepare the device by setting the class install parameters.
-	bool success = SetupDiSetClassInstallParams(devInfoSet, &devInfoData,
-	                                            reinterpret_cast<PSP_CLASSINSTALL_HEADER>(&propChangeParams),
-	                                            sizeof(propChangeParams));
+	std::string result(requiredBufferSize, '\0');
 
-	if (!success)
-	{
-		const DWORD error = GetLastError();
+	const int converted = WideCharToMultiByte(codePage,
+	                                          flags,
+	                                          wideString.data(),
+	                                          static_cast<int>(wideString.length()),
+	                                          result.data(),
+	                                          requiredBufferSize,
+	                                          NULL,
+	                                          NULL);
 
-		std::stringstream message;
-		message << "Failed to set class install parameters for device "
-			<< to_string(instanceId) << " with error code "
-			<< std::hex << std::setw(8) << std::setfill('0') << error;
+	wideToStringError(converted);
 
-		throw std::runtime_error(message.str());
-	}
-
-	// Apply the changes, disabling the device.
-	success = SetupDiCallClassInstaller(DIF_PROPERTYCHANGE, devInfoSet, &devInfoData);
-
-	if (!success)
-	{
-		const DWORD error = GetLastError();
-
-		std::stringstream message;
-		message << "Failed to disable device "
-			<< to_string(instanceId) << " with error code "
-			<< std::hex << std::setw(8) << std::setfill('0') << error;
-
-		throw std::runtime_error(message.str());
-	}
-
-	// Change state back to enabled.
-	propChangeParams.StateChange = DICS_ENABLE;
-
-	// Prepare the class install parameters again.
-	success = SetupDiSetClassInstallParams(devInfoSet, &devInfoData,
-	                                       reinterpret_cast<PSP_CLASSINSTALL_HEADER>(&propChangeParams),
-	                                       sizeof(propChangeParams));
-
-	if (!success)
-	{
-		const DWORD error = GetLastError();
-
-		std::stringstream message;
-		message << "Failed to set class install parameters for device "
-			<< to_string(instanceId) << " with error code "
-			<< std::hex << std::setw(8) << std::setfill('0') << error;
-
-		throw std::runtime_error(message.str());
-	}
-
-	// Apply the changes, enabling the device.
-	success = SetupDiCallClassInstaller(DIF_PROPERTYCHANGE, devInfoSet, &devInfoData);
-
-	if (!success)
-	{
-		const DWORD error = GetLastError();
-
-		std::stringstream message;
-		message << "Failed to enable device "
-			<< to_string(instanceId) << " with error code "
-			<< std::hex << std::setw(8) << std::setfill('0') << error;
-
-		throw std::runtime_error(message.str());
-	}
+	return result;
 }
 
 void toggleDevice(const std::wstring& instanceId)
@@ -117,11 +104,11 @@ void toggleDevice(const std::wstring& instanceId)
 	if (devInfoSet == nullptr || devInfoSet == INVALID_HANDLE_VALUE)
 	{
 		const DWORD error = GetLastError();
-
 		std::stringstream message;
+
 		message << "SetupDiGetClassDevs failed for device "
-			<< to_string(instanceId) << " with error code "
-			<< std::hex << std::setw(8) << std::setfill('0') << error;
+			<< wideToString(instanceId) << " with error code "
+			<< std::hex << std::setw(8) << std::setfill('0') << error << ": " << getLastErrorString(error);
 
 		throw std::runtime_error(message.str());
 	}
@@ -133,7 +120,96 @@ void toggleDevice(const std::wstring& instanceId)
 
 	try
 	{
-		toggleDevice_doWork(instanceId, devInfoSet);
+		SP_DEVINFO_DATA devInfoData;
+		devInfoData.cbSize = sizeof(SP_DEVINFO_DATA);
+
+		if (!SetupDiEnumDeviceInfo(devInfoSet, 0, &devInfoData))
+		{
+			const DWORD error = GetLastError();
+			std::stringstream message;
+
+			message << "Retrieving device info for instance ID "
+				<< wideToString(instanceId) << " failed while attempting to toggle device with error code "
+				<< std::hex << std::setw(8) << std::setfill('0') << error << ": " << getLastErrorString(error);
+
+			throw std::runtime_error(message.str());
+		}
+
+		SP_PROPCHANGE_PARAMS propChangeParams;
+
+		propChangeParams.ClassInstallHeader.cbSize          = sizeof(SP_CLASSINSTALL_HEADER);
+		propChangeParams.ClassInstallHeader.InstallFunction = DIF_PROPERTYCHANGE;
+
+		propChangeParams.StateChange = DICS_DISABLE;
+		propChangeParams.HwProfile   = 0;
+		propChangeParams.Scope       = DICS_FLAG_GLOBAL;
+
+		// Prepare the device by setting the class install parameters.
+		bool success = SetupDiSetClassInstallParams(devInfoSet, &devInfoData,
+		                                            reinterpret_cast<PSP_CLASSINSTALL_HEADER>(&propChangeParams),
+		                                            sizeof(propChangeParams));
+
+		if (!success)
+		{
+			const DWORD error = GetLastError();
+			std::stringstream message;
+
+			message << "Failed to set class install parameters for device "
+				<< wideToString(instanceId) << " with error code "
+				<< std::hex << std::setw(8) << std::setfill('0') << error << ": " << getLastErrorString(error);
+
+			throw std::runtime_error(message.str());
+		}
+
+		// Apply the changes, disabling the device.
+		success = SetupDiCallClassInstaller(DIF_PROPERTYCHANGE, devInfoSet, &devInfoData);
+
+		if (!success)
+		{
+			const DWORD error = GetLastError();
+			std::stringstream message;
+
+			message << "Failed to disable device "
+				<< wideToString(instanceId) << " with error code "
+				<< std::hex << std::setw(8) << std::setfill('0') << error << ": " << getLastErrorString(error);
+
+			throw std::runtime_error(message.str());
+		}
+
+		// Change state back to enabled.
+		propChangeParams.StateChange = DICS_ENABLE;
+
+		// Prepare the class install parameters again.
+		success = SetupDiSetClassInstallParams(devInfoSet, &devInfoData,
+		                                       reinterpret_cast<PSP_CLASSINSTALL_HEADER>(&propChangeParams),
+		                                       sizeof(propChangeParams));
+
+		if (!success)
+		{
+			const DWORD error = GetLastError();
+			std::stringstream message;
+
+			message << "Failed to set class install parameters for device "
+				<< wideToString(instanceId) << " with error code "
+				<< std::hex << std::setw(8) << std::setfill('0') << error << ": " << getLastErrorString(error);
+
+			throw std::runtime_error(message.str());
+		}
+
+		// Apply the changes, enabling the device.
+		success = SetupDiCallClassInstaller(DIF_PROPERTYCHANGE, devInfoSet, &devInfoData);
+
+		if (!success)
+		{
+			const DWORD error = GetLastError();
+			std::stringstream message;
+
+			message << "Failed to enable device "
+				<< wideToString(instanceId) << " with error code "
+				<< std::hex << std::setw(8) << std::setfill('0') << error << ": " << getLastErrorString(error);
+
+			throw std::runtime_error(message.str());
+		}
 	}
 	catch (const std::exception&)
 	{
